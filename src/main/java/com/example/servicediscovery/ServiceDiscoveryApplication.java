a3cc39da9521e6836090f1b4d094c6577dcdbc7c
Saptaparni Dey <saptaparnidey15@gmail.com>
2024-03-24 12:21:33 +0530

    Rename class name

--- src/main/java/com/example/servicediscovery/ServiceDiscoveryApplication.java
+++ src/main/java/com/example/servicediscovery/ServiceDiscoveryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicediscoveryApplication {
+public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicediscoveryApplication.class, args);
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 
 }
